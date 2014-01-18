58ad7107f5bf97afa3a1ea65f8d264d251f36896
juliusmercons <julius.masanting@orangeandbronze.com>
2014-01-18 16:15:54 +0800

    initial setup
    Signed-off-by: juliusmercons <julius.masanting@orangeandbronze.com>

---
+++ src/main/java/com/hospitalapp/drugmanager/dao/DrugDAO.java
@@ -0,0 +1,19 @@
+package com.hospitalapp.drugmanager.dao;
+
+
+import com.hospitalapp.drugmanager.domain.DrugCategory;
+
+import java.util.List;
+
+public interface DrugDAO {
+
+    void saveCategory(DrugCategory drugCategory);
+
+    void updateCategory(DrugCategory drugCategory);
+
+    void findCategoryById(Long id);
+
+    List<DrugCategory> listCategories();
+
+    void deleteCategory(DrugCategory drugCategory);
+}
---
+++ src/main/java/com/hospitalapp/drugmanager/dao/impl/DrugDAOImpl.java
@@ -0,0 +1,35 @@
+package com.hospitalapp.drugmanager.dao.impl;
+
+
+import com.hospitalapp.drugmanager.dao.DrugDAO;
+import com.hospitalapp.drugmanager.domain.DrugCategory;
+
+import java.util.List;
+
+public class DrugDAOImpl implements DrugDAO{
+
+    @Override
+    public void saveCategory(DrugCategory drugCategory) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void updateCategory(DrugCategory drugCategory) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void findCategoryById(Long id) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<DrugCategory> listCategories() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void deleteCategory(DrugCategory drugCategory) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
