0ad98ea4ddebe23b69f0812487cd0c20edd5ea91
juliusmercons <julius.masanting@orangeandbronze.com>
2014-03-22 19:39:54 +0800

    UserDTO: implemented Serializable
    Signed-off-by: juliusmercons <julius.masanting@orangeandbronze.com>

--- src/main/java/com/hospitalapp/uam/dto/UserDTO.java
+++ src/main/java/com/hospitalapp/uam/dto/UserDTO.java
@@ -1,6 +1,8 @@
 package com.hospitalapp.uam.dto;
 
-public class UserDTO {
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
 
     private String id;
     private String firstName;
